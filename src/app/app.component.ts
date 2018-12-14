0c4e3c9af1f43187d0e1fe69f84c8789216bbb18
ManevN <nenad.manev@gmail.com>
2018-12-14 16:18:51 +0100

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+  import {Component,Input} from "@angular/core";
+
+  @Component({
+    selector:"pm-root",
+    templateUrl:"./app.component.html"
+             
+  })
+
+  export class AppComponent{
+    pageTitle : string = "Acme Product Managment";
+
+
+  }
