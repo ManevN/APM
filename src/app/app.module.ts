3cd64089fa8b5ed78153c016125faba7c3f28a15
Nenad Manev <nenad.manev@hotmail.com>
2020-10-25 18:18:13 +0100

    update Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,14 +6,10 @@ import { AppComponent } from './app.component';
 import { WelcomeComponent } from './home/welcome.component';
 import { ProductModule } from './products/product.module';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import { MatCheckboxModule, 
-  MatRadioModule,
-  MatSelectModule,
-  MatInputModule} from '@angular/material';
-import { FormsModule } from '@angular/forms';
-
-
-
+import {MatCheckboxModule} from '@angular/material/checkbox'
+import {MatRadioModule} from '@angular/material/radio'
+import {MatSelectModule} from '@angular/material/select'
+import {MatInputModule} from '@angular/material/input'
 
 @NgModule({
   declarations: [
